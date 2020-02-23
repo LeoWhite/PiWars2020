3be619b79835d27affd96499342664cc8f2f8cc0
Leo White <leo.white@mybigideas.co.uk>
2020-02-23 11:13:36 +0000

    Added UI  to display the sensor details on

--- src/ToFSensorDisplay.py
+++ src/ToFSensorDisplay.py
@@ -5,6 +5,7 @@
 # Timings and setup taken from https://www.st.com/content/ccc/resource/technical/document/application_note/group1/13/fd/27/76/de/e8/46/4d/DM00566701/files/DM00566701.pdf/jcr:content/translations/en.DM00566701.pdf
 
 # Imports required for this porogram
+from guizero import App, Picture, Text
 import json
 import socket
 import sys
@@ -30,7 +31,8 @@ class UDPMonitorThread(Thread):
           data, addr = self.socket.recvfrom(1024)
 
           message = json.loads(data)
-          print(message["name"], message["distance"], message["time"])
+          if message["name"] == "front_left":
+            self._FLText.value = message["distance"]
 
 # Details on the ToF sensors we are monitoring
 ToFSensors = [ ]
@@ -43,12 +45,19 @@ with open(JSON_CONFIG_FILE) as json_data_file:
 # Create a socket to broadcast the messages on
 udpAddress = ( config["ToF"]["udp"]["address"], int(config["ToF"]["udp"]["port"]) )
 
+
+# Create the GUI to display the sensor details
+app = App(title="Thunderbird 2", layout="grid")
+top_message = Text(app, text="ToF Sensor Output", grid=[1,0])
+front_left_message = Text(app, text="0.0", grid=[1,1], align="left")
+front_right_message = Text(app, text="0.0", grid=[1,1], align="right")
+left_message = Text(app, text="0.0", grid=[0,2], align="right")
+tb2 = Picture(app, image="TB2Top.png", grid=[1,2])
+right_message = Text(app, text="0.0", grid=[2,2], align="left")
+
 # Read in the data
-udpMonitor = UDPMonitorThread(udpAddress, 0, 0, 0, 0)
+udpMonitor = UDPMonitorThread(udpAddress, front_left_message, front_right_message, left_message, right_message)
 udpMonitor.daemon = True
 udpMonitor.start()
 
-running = True
-
-while running:
-  time.sleep(5)
+app.display()
