74e1c0a109ee1afdd25519b5750d1ca32325b255
Leo White <leo.white@mybigideas.co.uk>
2020-02-23 11:00:12 +0000

    Added class to monitor for UDP packets in the background

--- src/ToFSensorDisplay.py
+++ src/ToFSensorDisplay.py
@@ -8,32 +8,47 @@
 import json
 import socket
 import sys
+from threading import Thread
+import time
 
 # Defines and constants
 JSON_CONFIG_FILE='/home/pi/Programming/PiWars2020/config/tb2.json'
 
+# Thread that monitors for UDP packets in the background
+class UDPMonitorThread(Thread):
+    def __init__(self, UDPAddress, FLText, FRText, LText, RText):
+        Thread.__init__(self)
+        self._FLText = FLText
+        self._FRText = FRText
+        self._LText = LText
+        self._RText = RText
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(UDPAddress)
+
+    def run(self):
+        while True:
+          data, addr = self.socket.recvfrom(1024)
+
+          message = json.loads(data)
+          print(message["name"], message["distance"], message["time"])
+
 # Details on the ToF sensors we are monitoring
 ToFSensors = [ ]
 
-# Create the 
 # Read in the JSON config file
 with open(JSON_CONFIG_FILE) as json_data_file:
     config = json.load(json_data_file)
-print(config)
 
 
 # Create a socket to broadcast the messages on
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udpAddress = ( config["ToF"]["udp"]["address"], int(config["ToF"]["udp"]["port"]) )
-sock.bind(udpAddress)
 
 # Read in the data
+udpMonitor = UDPMonitorThread(udpAddress, 0, 0, 0, 0)
+udpMonitor.daemon = True
+udpMonitor.start()
+
 running = True
 
 while running:
-  results = []
-  data, addr = sock.recvfrom(1024)
-
-  message = json.loads(data)
-  print(message["name"], message["distance"], message["time"])
-  #time.sleep(0.05)
+  time.sleep(5)
