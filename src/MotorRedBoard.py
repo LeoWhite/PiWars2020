cd2bea0abf5519e8ad22d9d78f00bb43ceaea76f
Leo White <leo.white@mybigideas.co.uk>
2021-01-01 11:31:02 +0000

    Switched over to ApprocEng's RedBoard library
    https://github.com/ApproxEng/RedBoard

--- src/MotorRedBoard.py
+++ src/MotorRedBoard.py
@@ -1,26 +1,25 @@
 #! /usr/bin/python3
 from MotorBase import MotorBase
 import sys
-sys.path.append('/home/pi/RedBoard')
 import redboard
 
 # A Motor class used for abstraction purposes
 
 class MotorRedBoard(MotorBase):
     def __init__(self):
-        # Redboard doesn't have any setup stage currently, so nothing to do here!
         print("RedBoard init")
+        self._redBoard = redboard.RedBoard()
 
     def _left(self, speed):
         """ Sets the speed of the left motor, in the range of -1.0 to 1.0"""
         #print("_left"+str(speed))
-        redboard.M2(speed * 100)
+        self._redBoard.m1 = speed
 
 
     def _right(self, speed):
         """ Sets the speed of the right motor, in the range of -1.0 to 1.0"""
         #print("_right"+str(speed))
-        redboard.M1(speed * 100)
+        self._redBoard.m0 = speed
         
 
         
